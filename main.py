6fcf672def21a71d8b977e65e459eb30766a7098
Leandro <vilases@hotmail.com>
2024-06-14 20:23:11 -0300

    Tercer commit.
    Importacion modulo matplotlib.
    Obtencion del dataframe, cambio de dtypes y visualizacion

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from modulos.obtenerDatos import cargar_sheet
+import matplotlib.pyplot as plt
 
 data = cargar_sheet()
 df = pd.DataFrame (data[1:], columns = data[0])
+df['transactions'] = df['transactions'].astype(int)
+df['gross_margin'] = df['gross_margin'].astype(float)
+print(df.dtypes)
+
+df['gross_margin'].plot.hist(bins = 20)
 print (df)
+plt.show()
